1ce7ecb2bb34ab8f46f362944e7260d09e075fdd
Ronith <44728044+Ronith900@users.noreply.github.com>
2021-12-31 18:59:50 +0800

    minor Enhancements

---
+++ components/drawer.vue
@@ -0,0 +1,66 @@
+<template>
+  <v-navigation-drawer
+    v-model="drawer"
+    :mini-variant="miniVariant"
+    clipped="true"
+    fixed
+    app
+  >
+    <v-list>
+      <v-list-item
+        v-for="(item, i) in items"
+        :key="i"
+        :to="item.to"
+        router
+        exact
+      >
+        <v-list-item-action>
+          <v-icon>{{ item.icon }}</v-icon>
+        </v-list-item-action>
+        <v-list-item-content>
+          <v-list-item-title v-text="item.title" />
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from "nuxt-property-decorator";
+
+@Component
+export default class ESDrawer extends Vue {
+  private items: Array<object> = [
+    {
+      icon: "mdi-apps",
+      title: "Welcome",
+      to: "/",
+    },
+    {
+      icon: "mdi-chart-bubble",
+      title: "Inspire",
+      to: "/inspire",
+    },
+    {
+      icon: "mdi-chart-bubble",
+      title: "Groups",
+      to: "/groups",
+    },
+    {
+      icon: "mdi-chart-bubble",
+      title: "Friends",
+      to: "/friends",
+    },
+    {
+      icon: "mdi-chart-bubble",
+      title: "Activity",
+      to: "/activities",
+    },
+    {
+      icon: "mdi-chart-bubble",
+      title: "Account",
+      to: "/account",
+    },
+  ];
+}
+</script>
